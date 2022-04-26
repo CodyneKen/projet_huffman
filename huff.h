e77e202bb68c939bc427dddb145d6f5a3cad585d
CodyneKen <corentinbohelay@gmail.com>
2022-04-26 21:29:57 +0200

    creation du projet

---
+++ huff.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*max number of chars able to be encoded by the algo*/
+#define N_CHAR 256
+/*max number of bits for encoding*/
+#define BIT_ENC 256
+
+typedef struct noeud {
+  char c;
+  int occ;
+  int enc[BIT_ENC];
+  int bit_num;
+  struct noeud *gauche;
+  struct noeud *droite;
+}
+
+void occurence(FILE *fic, int tab[256]);
+
+
---
+++ huff_v0.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv){
+        
+  FILE* f;
+  char c = '\0';
+
+  if (argc<2){
+    printf("Merci de fournir un nom de fichier\n");
+    exit(EXIT_FAILURE);
+  }
+/* on essaye d'ouvrir le fichier, erreur sinon :*/
+  if ((f = fopen(argv[1], "r"))){
+      while ( (c = fgetc(f)) != EOF )
+        printf("%c", c);
+  }
+}
